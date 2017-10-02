7208fd4c1f4c110afe27e5371b76834ccc5b71ce
Chris Hawk <chrishawk9@gmail.com>
2017-10-02 11:31:52 -0600

    create a table w/ hard-coded num of rows and cols when user clicks submit

--- designs.js
+++ designs.js
@@ -2,20 +2,30 @@
 // Select size input
 
 let submit = $('form').children().last();
-submit.on('click', makeGrid(submit.	event));
+submit.on('click', makeGrid);
 
-function makeGrid(event) {
-	event.preventDefault();
-	let rows, cols, color;
+function makeGrid() {
+	$('body').append('<p>"Hello"</p>');
+	let rows, cols, currentRow, currentCell, color, canvas;
 
 	rows = $('#input_height').attr('value');
 	console.log('rows:' + rows);
 	cols = $('#input_width').attr('value');
 	console.log('cols:' + cols);
-// for each row
-	// for each col
-		//make cell
 
+	rows = 5;
+	cols = 5;
+	canvas = $('#pixel_canvas');
+	for(let i = 0; i < rows; i++) {
+		currentRow = document.createElement('tr');
+		canvas.append(currentRow);
+		for(let j = 0; j < rows; j++) {
+			currentCell = document.createElement('td');
+			currentRow.append(currentCell);
+		}
+	}
+	console.log(canvas);
+	return false;
 }
 
 //Called when the user clicks on a cell in the grid. Causes the background color of that cell to change to the chosen color.
